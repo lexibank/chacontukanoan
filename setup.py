8348235fbbf05b21b4d8d108b82a3e9c7f02c1f6
sbeniamine <beniamine@shh.mpg.de>
2020-07-22 09:57:12 +0200

    Init with cldfbench new

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import json
+
+
+with open('metadata.json', encoding='utf-8') as fp:
+    metadata = json.load(fp)
+
+
+setup(
+    name='lexibank_chacontukanoan',
+    description=metadata['title'],
+    license=metadata.get('license', ''),
+    url=metadata.get('url', ''),
+    py_modules=['lexibank_chacontukanoan'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'lexibank.dataset': [
+            'chacontukanoan=lexibank_chacontukanoan:Dataset',
+        ]
+    },
+    install_requires=[
+        'pylexibank>=2.7.0',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
